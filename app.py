047acf072dd3f2294349de6e75c3ce79d1498317
Gabriel Navarro <gchinonavarro@gmail.com>
2025-05-03 07:15:41 -0700

    SVG edit

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ from fasthtml.common import fast_app, serve, Title, Main
 from src.pages import HERO_PAGE, MASONRY_PAGE, CV_PAGE, create_blog_page
 import argparse
 
-# Parse command line argument for port
-parser = argparse.ArgumentParser(description="Run the FastHTML app.")
-parser.add_argument("--port", type=int, default=80, help="Port to run the app on")
-args = parser.parse_args()
-
 # This is a simple FastHTML app that serves a page with a title and a main content area.
 app, rt = fast_app()
 
@@ -28,5 +23,10 @@ def get(blog_id: str):
 @rt("/cv")
 def cv():
     return Title("Gabriel - CV"), CV_PAGE
+
+# Parse command line argument for port
+parser = argparse.ArgumentParser(description="Run the FastHTML app.")
+parser.add_argument("--port", type=int, default=80, help="Port to run the app on")
+args = parser.parse_args()
 
 serve(port=args.port, reload=False)
